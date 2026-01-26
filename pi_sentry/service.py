9be079e7cecb4ea0677cc2c6a1c2e8679ed6ada1
Ian Waldron <ian@waldrontech.com>
2026-01-26 14:16:34 -0800

    Update service.py with an env var that toggles autostart on power on

--- pi_sentry/service.py
+++ pi_sentry/service.py
@@ -1,6 +1,8 @@
 """Systemd service entrypoint for Pi Sentry."""
 
 import logging
+import os
+import sys
 
 from config import LOG_FORMAT, LOG_DATE_FORMAT, LOG_LEVEL
 from led import StatusLED
@@ -9,10 +11,16 @@ from main import main
 logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
 logger = logging.getLogger(__name__)
 
+PI_SENTRY_ENABLED = os.environ.get("PI_SENTRY_ENABLED", "1") == "1"
+
 
 def start():
     """Service entrypoint - startup sequence then main loop."""
-    logger.info("Py Sentry starting")
+    if not PI_SENTRY_ENABLED:
+        logger.info("Pi Sentry disabled via PI_SENTRY_ENABLED=0")
+        sys.exit(0)
+
+    logger.info("Pi Sentry starting")
 
     # Startup blink pattern - 5 quick blinks
     with StatusLED() as led:
